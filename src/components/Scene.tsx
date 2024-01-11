8a72228afbd3438fe1b3ff05ad979bef850d9592
Neel Dandiwala <neeldandi@gmail.com>
2024-01-11 02:25:15 -0500

    added lower podium

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -4,55 +4,54 @@ import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import type { GLTF } from "three-stdlib";
 
-type GLTFResult = GLTF & {
-    nodes: { 
-        Cylinder: THREE.Mesh;
+type LowerPodiumType = GLTF & {
+    nodes: {
+      Cylinder: THREE.Mesh;
+      PlantZZ001002: THREE.Mesh;
     };
     materials: {
-        ["Material.002"]: THREE.MeshStandardMaterial;
+      ["Material.002"]: THREE.MeshStandardMaterial;
+      MyGold: THREE.MeshStandardMaterial;
     };
-};
-
-const Model = (props: JSX.IntrinsicElements["group"]) => {
-    const { controls } = useThree();
-  const meshRef = useRef();
-
-  const margin = 0.2;
-  useEffect(() => {
-    if (controls) {
-      controls.fitToBox(meshRef.current, true, {
-        paddingTop: margin,
-        paddingLeft: margin,
-        paddingBottom: margin,
-        paddingRight: margin,
-      });
-      controls.rotateTo(Math.PI / -0.4, Math.PI / 2.5, true);
-    }
-  }, [controls]);
-    const { nodes, materials } = useGLTF("models/lower_podium.glb") as GLTFResult;
-    
+  };
+  
+const LowerPodium = (props: JSX.IntrinsicElements["group"]) => {
+    const { nodes, materials } = useGLTF("models/trial4.glb") as LowerPodiumType;
     return (
-        <group {...props} dispose={null}>
-            <mesh ref={meshRef}
-                castShadow
-                receiveShadow
-                geometry={nodes.Cylinder.geometry}
-                material={materials["Material.002"]}
-            />
-            <CameraControls makeDefault />
-        </group>
+      <group {...props} dispose={null}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cylinder.geometry}
+          material={materials["Material.002"]}
+          position={[0.08, -0.185, 0.018]}
+          scale={0.669}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.PlantZZ001002.geometry}
+          material={materials.MyGold}
+          position={[-1.042, 0.368, -0.528]}
+          rotation={[-0.478, -0.124, 0.588]}
+          scale={1.749}
+        />
+      </group>
     );
-}
-
-useGLTF.preload("models/lower_podium.glb");
+  }
+  
+  useGLTF.preload("models/trial4.glb");
 
 export const Fiber = () => {
+    
+    
     return (
       <Canvas
         camera={{ position: [10, -20, 5], fov: 45 }}
         style={{ height: "100%" }}
       >
-        <Model />
+        <LowerPodium />
+        <CameraControls makeDefault />
       </Canvas>
     );
   };
